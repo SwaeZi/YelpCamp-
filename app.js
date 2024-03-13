a36341cec1f36d83439656bbbed6ae76d3f3657e
SwaeZi <stevenstefanov@icloud.com>
2024-03-13 16:23:52 +0100

    add: ability to add new campground, UI buttons on every page

--- app.js
+++ app.js
@@ -15,9 +15,12 @@ db.once("open", () => {
     console.log("Database Connected")
 });
 
-//Folders 
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({extended: true}))
+
 
 //Show the Info
 app.get('/', (req,res) => {
@@ -28,6 +31,17 @@ app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render('campgrounds/index', { campgrounds })
 });
+
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
 
 app.get('/campgrounds/:id', async (req,res) => {
     const campground = await Campground.findById(req.params.id)
