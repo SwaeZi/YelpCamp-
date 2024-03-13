db62235428d910c7eef1b49a8a33db2e374ced7d
SwaeZi <stevenstefanov@icloud.com>
2024-03-13 13:52:14 +0100

    create,connect, test the database

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
 
+//Database Connect
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+    console.log("Database Connected")
+});
+
+//Folders 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//Show the Info
 app.get('/', (req,res) => {
-    res.send('HELLO FROM YELP CAMP')
+    res.render('home')
 })
 
+//Local Host 
 app.listen(3000, () => {
     console.log('SERVING ON PORT 3000')
 })
