d3c347a06a9e7118aa3fb0ade8348ea1a9244222
SwaeZi <stevenstefanov@icloud.com>
2024-03-24 14:04:17 +0100

    add: validate and show errors before submitting a form

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const engine = require('ejs-mate');
+const Joi = require('joi');
+const { campgroundSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require("method-override");
 const Campground = require('./models/campground');
 const { error } = require('console');
 
+
+const validateCampground = (req, res, next) => {
+        const { error } = campgroundSchema.validate(req.body);
+        if(error) {
+            const msg = error.details.map(el => el.message).join(',')
+            throw new ExpressError(msg, 400)
+        } else {
+            next();
+        }
+}
 
 
 //Database Connect
@@ -45,8 +57,8 @@ app.get("/campgrounds/new", (req, res) => {
   res.render("campgrounds/new");
 });
 
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
-    if (!req.body.campground) throw new ExpressError('INVALID CAMPGROUND DATA', 400);
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+    //if (!req.body.campground) throw new ExpressError('INVALID CAMPGROUND DATA', 400);//
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
@@ -63,7 +75,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground });
 }));
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
     res.redirect(`/campgrounds/${campground._id}`)
